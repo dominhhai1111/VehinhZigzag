42b94bc1c8fdcf725397749ce5fa8d59922cee10
Do Minh Hai <dominhhai@MacBook-Air.local>
2015-08-08 17:22:38 +0700

    Initial Commit

---
+++ ziczax2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ziczax2
+//
+//  Created by Do Minh Hai on 8/8/15.
+//  Copyright (c) 2015 Do Minh Hai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
